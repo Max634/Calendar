44a9c2f7a28dfb71c2c4f4b40945093b9778501f
Maksim Botukh <75707184+Max634@users.noreply.github.com>
2021-06-18 13:50:46 -0500

    Add files via upload

---
+++ Calendar.cpp
@@ -0,0 +1,99 @@
+/*
+****  CALENDAR ***
+1. Number of Days in a Month
+2. Offset day (0 for Monday - 6 for Sunday)
+
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int getInputDays()
+{
+	int inputDays = 0;
+	bool validInput = false;
+	while (validInput == false);
+	{
+		cout << "\n Please Enter number of Days in the Month";
+		cin >> inputDays;
+		
+		if (inputDays >= 28 && inputDays <= 31)
+			validInput = true;
+	}
+	return inputDays;
+
+
+}
+int getInputOffset()
+{
+	int inputOffset = 0;
+	bool validInput = false;
+	while (validInput == false);
+	{
+		cout << "\n Please Enter the Offset number";
+		cin >> inputOffset;
+
+		if (inputOffset >= 0 && inputOffset <= 6)
+			validInput = true;
+	}
+	if (inputOffset == 6)
+		inputOffset = 0;
+	else
+		inputOffset == 1;
+		return inputOffset;
+}
+void displayOffset(int offset)
+{
+	if (offset == 1)
+		cout << "     1";
+	if (offset == 2)
+		cout << "            1";
+	if (offset == 3)
+		cout << "                1";
+	if (offset == 4)
+		cout << "                    1"; 
+	if (offset == 5)
+		cout << "                        1";
+}
+
+void drawCalendar(int days, int offset)
+{
+	int date = 1;
+	int spaces = 0;
+	cout << "\n\n\t C A L E N D A R\n\n Su Mo Tu We Th Fr Sa" << endl;
+	while (date <= days)
+	{
+		if (offset > 0)
+		{
+			displayOffset(offset);
+			date = 2;
+			spaces = (offset * 4) + 4;
+			offset = 0;
+		}
+		while (spaces < 28)
+		{
+			if (date >= 10)
+			{
+				if (date > days)
+					break;
+				cout << " " << date;
+			}
+
+			else
+				cout << " " << date;
+			date += 1;
+			spaces += 4;
+		}
+		cout << endl;
+		spaces = 0;
+	}
+}
+int main()
+{
+	int days = getInputDays();
+	int offset = getInputOffset();
+	drawCalendar(days, offset);
+	cout << endl;
+	return 0;
+}
